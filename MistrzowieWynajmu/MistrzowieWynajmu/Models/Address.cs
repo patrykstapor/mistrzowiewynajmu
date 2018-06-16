825e07eba5771a41fbf56dccc8081c866d719aec
patrykstapor <patrykstapor16@gmail.com>
2018-06-16 19:42:00 +0200

    Models

---
+++ MistrzowieWynajmu/MistrzowieWynajmu/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MistrzowieWynajmu.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ MistrzowieWynajmu/MistrzowieWynajmu/Models/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MistrzowieWynajmu.Models
+{
+    public class Owner
+    {
+        public int OwnerId { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Phone { get; set; }
+    }
+}
